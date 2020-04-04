9e827603d4950e0f56c1bec619b8eb1793a004e2
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-04 19:25:22 +0000

    Automatically backed up by Learn

--- db/migrate/007_update_shows.rb
+++ db/migrate/007_update_shows.rb
@@ -1,4 +1,4 @@
-Class UpdateShows < ActiveRecord::Migration[5.1]
+class UpdateShows < ActiveRecord::Migration[5.1]
     def change 
         add_column :shows, :day, :string
         add_column :shows, :genre, :string
