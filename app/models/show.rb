da3818489dcfd855da2f44f3e06b2bf08a4cc0ba
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-04 19:33:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,8 @@ class Show < ActiveRecord::Base
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
   end  
-  # def actors_list
-  #   self.full_name.all
-  # end
+  def actors_list
+    self.full_name.all
+  end
 
 end
