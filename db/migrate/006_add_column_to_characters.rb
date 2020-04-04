3008489654811683737f2567e3caa913351f0800
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-04 19:22:55 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_column_to_characters.rb
+++ db/migrate/006_add_column_to_characters.rb
@@ -1,4 +1,4 @@
-class AddColumnToCharacters < ActiveRecord::Migration[5.2]
+class AddColumnToCharacters < ActiveRecord::Migration[5.1]
  
   def change
     add_column :characters, :catchphrase, :string
