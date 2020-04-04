5ab154b0c6e584f3dd3dc5e812df09e90d3fd224
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-04 19:31:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
   belongs_to :network
-  has_many :characters
+  has_many :characters 
+  # hass many though here
   def sorry
     "We're sorry about passing on John Mulaney's pilot"
   end  
